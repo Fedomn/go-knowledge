01b7a170bd9775e83ed1f6061025761800af1a06
Fedomn <shadowforfiend@gmail.com>
2018-05-10 10:49:33 +0800

    add container doc

---
+++ test/container_test.go
@@ -0,0 +1,16 @@
+package main
+
+// heap.go doc:
+// A heap is a tree with the property that each node is the minimum-valued node in its subtree.
+// The minimum element in the tree is the root, at index 0.
+// A heap is a common way to implement a priority queue.
+
+// heap主要用于构造 优先队列 始终保证第一个元素的优先级最高
+// 参考 src/container/heap/example_pq_test.go
+
+// list.go doc:
+// list implements a doubly linked list.
+
+// ring.go doc:
+// ring implements operations on circular lists.
+// 具体方法 example https://golang.org/pkg/container/ring/#Ring
