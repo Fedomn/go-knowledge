f80fe8b3a80d2ea0c2cd7aab2c498fb98a283a3e
Fedomn <shadowforfiend@gmail.com>
2018-05-17 22:48:46 +0800

    add redis limit example

---
+++ test/gin/limit/limiter.go
@@ -0,0 +1,70 @@
+package limit
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"net/http"
+	"time"
+)
+
+// 总体参考：https://www.cnblogs.com/AndyAo/p/8144049.html
+
+// 1、限制瞬时并发数
+// copy from https://github.com/gin-gonic/contrib gin-limit
+// 防止一次性建立过多连接 消耗大量资源 反而性能下降
+func MaxAllowed(n int) gin.HandlerFunc {
+	sem := make(chan struct{}, n)
+	acquire := func() { sem <- struct{}{} }
+	release := func() { <-sem }
+	return func(c *gin.Context) {
+		acquire()       // before request
+		defer release() // after request
+		c.Next()
+	}
+}
+
+// 2、限制时间窗最大请求数
+// copy from https://medium.com/@salvatoregiordanoo/a-step-approach-to-rate-limiting-f2190dff9fd4
+// 请求都是过来了的 限流只是不进行业务逻辑处理直接返回
+// 想象成一条管道 左进右出。窗口是以左边(now)开始，向右边走(slidingWindow)距离
+func NewRateLimiterMiddleware(redisClient *redis.Client, key string, limit int, slidingWindow time.Duration) gin.HandlerFunc {
+
+	_, err := redisClient.Ping().Result()
+	if err != nil {
+		panic(fmt.Sprint("error init redis", err.Error()))
+	}
+
+	return func(c *gin.Context) {
+		now := time.Now().UnixNano()
+		userCntKey := fmt.Sprint(c.ClientIP(), ":", key)
+
+		// 移除有序集 key 中，所有 score 值介于 min 和 max 之间的成员
+		// 移除窗口以外的元素
+		redisClient.ZRemRangeByScore(userCntKey,
+			"0",
+			fmt.Sprint(now-(slidingWindow.Nanoseconds()))).Result()
+
+		// 按照score从低到高的顺序返回范围内的元素
+		// 返回窗口内的元素
+		reqs, _ := redisClient.ZRange(userCntKey, 0, -1).Result()
+
+		if len(reqs) >= limit {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"status":  http.StatusTooManyRequests,
+				"message": "too many request",
+			})
+			return
+		}
+
+		c.Next()
+
+		// 只添加新元素，不会更新已有元素
+		// 向窗口内插入元素
+		redisClient.ZAddNX(userCntKey, redis.Z{Score: float64(now), Member: float64(now)})
+
+		// 重新设置TTL
+		// 这里并不会对限速产生影响，它的存在是为了 自动删除这个key
+		redisClient.Expire(userCntKey, slidingWindow)
+	}
+}
---
+++ test/gin/limit/limiter_test.go
@@ -0,0 +1,53 @@
+package limit_test
+
+import (
+	. "fedomn/go-knowledge/test/gin/limit"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+	"net/http"
+	"testing"
+	"time"
+)
+
+// SortedSet命令 http://redisdoc.com/sorted_set/index.html
+// ZRANGE 127.0.0.1:test 0 -1 WITHSCORES
+
+func TestRedisLimiter(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	slidingWindow := time.Second
+
+	r.Use(NewRateLimiterMiddleware(redis.NewClient(
+		&redis.Options{
+			DB:       0,
+			Password: "",
+			Addr:     "127.0.0.1:6379",
+		},
+	), "test", 20, slidingWindow))
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "OK")
+	})
+
+	go r.Run(":9999")
+
+	for i := 0; i < 100; i++ {
+		c := &http.Client{}
+
+		resp, e := c.Get("http://127.0.0.1:9999")
+		if e != nil {
+			t.Error("Error during requests ", e.Error())
+			return
+		}
+
+		if resp.StatusCode == http.StatusTooManyRequests {
+			//fmt.Printf("too many requests: %v\n", resp.StatusCode)
+		}
+
+		if i == 50 {
+			// 等待目的：为了从now开始 向前推slidingWindow 窗口内的请求数减少，以便可以继续发请求
+			time.Sleep(time.Second * 2)
+		}
+	}
+}
