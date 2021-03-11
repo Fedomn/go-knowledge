765420abce3bffff15277244345f031f1dbb9e9a
Fedomn <shadowforfiend@gmail.com>
2021-03-11 22:27:52 +0800

    add sort search test

---
+++ test/sort_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+func Test_SortSearch(t *testing.T) {
+	nodes := []int{1, 5, 2, 4, 3, 1, 2}
+
+	find := 2
+	sort.Ints(nodes)
+	idx, present := searchFirstLargerIdx(nodes, find)
+	fmt.Printf("nodes: %v, find: %d, idx: %d, present: %t\n", nodes, find, idx, present)
+
+	find = 12345
+	sort.Ints(nodes)
+	idx, present = searchFirstLargerIdx(nodes, find)
+	fmt.Printf("nodes: %v, find: %d, idx: %d, present: %t\n", nodes, find, idx, present)
+
+	find = 2
+	nodes = []int{}
+	idx, present = searchFirstLargerIdx(nodes, find)
+	fmt.Printf("nodes: %v, find: %d, idx: %d, present: %t\n", nodes, find, idx, present)
+}
+
+func searchFirstLargerIdx(nodes []int, find int) (int, bool) {
+	idx := sort.Search(len(nodes), func(i int) bool {
+		return nodes[i] >= find
+	})
+	return idx, idx < len(nodes) && nodes[idx] == find
+}
