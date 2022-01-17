af2b991eaecf8366835bd75993471c50afd3a0ac
Fedomn <fedomn.ma@gmail.com>
2022-01-17 10:14:22 -0600

    add generic example

---
+++ test/generic_test.go
@@ -0,0 +1,34 @@
+//go:build generic_enable
+
+// refer to https://go.dev/doc/tutorial/generics
+
+package main
+
+import "testing"
+
+type Number interface {
+	int64 | float64
+}
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64 as types for map values.
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func TestGeneric(t *testing.T) {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+}
