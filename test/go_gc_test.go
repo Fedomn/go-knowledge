c3ccb7b77cb17f2bc300dc1c1405a5837ff8e773
Fedomn <fedomn.ma@gmail.com>
2022-01-17 10:47:15 -0600

    enhance go gc test

--- test/go_gc_test.go
+++ test/go_gc_test.go
@@ -7,6 +7,23 @@ import (
 	"time"
 )
 
+// original idea from https://eng.uber.com/how-we-saved-70k-cores-across-30-mission-critical-services/
+// 目的：
+// Garbage collection in Go is concurrent and involves analyzing all objects to identify which ones are still reachable.
+// We would call the reachable objects the "live dataset."
+// Go offers only one knob, GOGC, expressed in percentage of live dataset, to control garbage collection
+// hard_target = live_dataset + live_dataset * (GOGC / 100).
+
+// issue:
+// It is not aware of the maximum memory assigned to the container and can cause out of memory issues.
+// Our microservices have a significantly diverse memory utilization portfolio. 100MB instances were having a huge GC impact.
+
+// 解决：
+// GOGCTuner dynamically computes the correct GOGC value in accordance with the container’s memory limit and sets it using Go’s runtime API.
+// Go forces a garbage collection every 2 minutes.
+
+// https://github.com/cch123/gogctuner
+
 type Wrapper int
 
 func findWrapper(r *Wrapper) {
